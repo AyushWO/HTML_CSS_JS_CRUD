61152789b1522395552f83e68ee08cbbfc3cf3ae
ayushwo <ayushm1.webosmotic@gmail.com>
2024-04-12 10:52:45 +0530

    submain

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-// Function to submit the form
 function submitForm(event) {
   event.preventDefault();
-  if (getAge()) { // Check age validation before submitting the form
+  if (getAge()) {
     const id = document.getElementById("id").value;
     const name = document.getElementById("name").value;
     const skills = getSelectedSkills();
     const salary = document.getElementById("salary").value;
     const dob = document.getElementById("dob").value;
-
-    if (name && skills.length > 0 && salary && dob) {
+    validationMessage('id');
+    validationMessage('name');
+    validationMessage('salary');
+    validationMessage('dob');
+    if (name && salary && dob) {
       const employee = {
         id: id,
         name: name,
@@ -23,55 +25,61 @@ function submitForm(event) {
   }
 }
 
-// Function to display validation messages
-function validationMessage() {
-  const id = document.getElementById("id").value;
-  const name = document.getElementById("name").value;
-  const skills = getSelectedSkills();
-  const salary = document.getElementById("salary").value;
-  const dob = document.getElementById("dob").value;
+function validationMessage(inputId) {
+  let errorMessage = "";
 
-  const errorMessageId = (id === "" || id === null || !/^\d+$/.test(id)) ? "(Please Enter Id with Digits)" : "";
-  const errorMessageName = (name === "" || /\d/.test(name)) ? "(Please Enter Name without Numbers)" : "";
-  const errorMessageSkills = (skills.length === 0) ? "(Please select at least one Skill)" : "";
-  const errorMessageSalary = (salary == "" || isNaN(salary)) ? "(Please Enter Salary)" : "";
-  const errorMessageDob = (dob == "") ? "(Please Enter Date of Birth)" : "";
+  if (inputId === "id") {
+    const id = document.getElementById("id").value;
+    if (id === "" || id === null || !/^\d+$/.test(id)) {
+      errorMessage = "(Please Enter Id with Digits)";
+    }
+    document.getElementById("errorId").innerHTML = errorMessage;
+  } else if (inputId === "name") {
+    const name = document.getElementById("name").value;
+    if (name === "" || /\d/.test(name)) {
+      errorMessage = "(Please Enter Name without Numbers)";
+    }
+    document.getElementById("errorName").innerHTML = errorMessage;
+  } else if (inputId === "salary") {
+    const salary = document.getElementById("salary").value;
+    if (salary === "" || isNaN(salary)) {
+      errorMessage = "(Please Enter Salary)";
+    }
+    document.getElementById("errorSalary").innerHTML = errorMessage;
+  } else if (inputId === "dob") {
+    const dob = document.getElementById("dob").value;
+    if (dob === "") {
+      errorMessage = "(Please Enter Date of Birth)";
+    }
+    document.getElementById("errorDob").innerHTML = errorMessage;
+  }
 
-  document.getElementById("errorId").innerHTML = errorMessageId;
-  document.getElementById("errorName").innerHTML = errorMessageName;
-  document.getElementById("errorSkills").innerHTML = errorMessageSkills;
-  document.getElementById("errorSalary").innerHTML = errorMessageSalary;
-  document.getElementById("errorDob").innerHTML = errorMessageDob;
-
-  // If validation passed, return true, else false
-  return !(errorMessageId || errorMessageName || errorMessageSkills || errorMessageSalary || errorMessageDob);
+  return errorMessage;
 }
 
-// Function to calculate age
 function getAge() {
-  const lblError = document.getElementById("errorDob");
+  const dobSmallError = document.getElementById("errorDob");
   const dateString = document.getElementById("dob").value;
 
   const parts = dateString.split("-");
-  const dtDOB = new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2])); // Create a Date object with the provided date
-  const dtCurrent = new Date(); // Get current date
-  let age = dtCurrent.getFullYear() - dtDOB.getFullYear(); // Calculate age difference
+  const dtDOB = new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]));
+  const dtCurrent = new Date();
+  let age = dtCurrent.getFullYear() - dtDOB.getFullYear();
 
   if (dtCurrent.getMonth() < dtDOB.getMonth() || (dtCurrent.getMonth() === dtDOB.getMonth() && dtCurrent.getDate() < dtDOB.getDate())) {
-    age--; // Adjust age if birthday hasn't occurred yet this year
+    age--; 
   }
 
   if (age < 18) {
-    lblError.innerHTML = "Age should be 18 or above.";
+    dobSmallError.innerHTML = "Age should be 18 or above.";
     return false;
   } else {
-    lblError.innerHTML = "";
+    dobSmallError.innerHTML = "";
     return true;
   }
 }
 
 
-// Function to get selected skills
 function getSelectedSkills() {
   const skills = [];
   const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
@@ -81,15 +89,12 @@ function getSelectedSkills() {
   return skills;
 }
 
-// Function to add or update an employee
 function addOrUpdateEmployee(employee) {
   const tableBody = document.getElementById("tableBody");
   let existingEmployee = false;
 
-  // Check if an employee with the same ID already exists
   tableBody.querySelectorAll("tr").forEach((row) => {
     if (row.cells[0].textContent === employee.id.toString()) {
-      // If employee exists, update their information
       row.cells[1].textContent = employee.name;
       row.cells[2].textContent = employee.skills.join(", ");
       row.cells[3].textContent = employee.salary;
@@ -99,7 +104,6 @@ function addOrUpdateEmployee(employee) {
     }
   });
 
-  // If employee doesn't exist, add a new one
   if (!existingEmployee) {
     const row = tableBody.insertRow();
     row.innerHTML = `
@@ -114,7 +118,6 @@ function addOrUpdateEmployee(employee) {
   }
 }
 
-// Function to save employees to local storage
 function updateLocalStorage() {
   const tableBody = document.getElementById("tableBody");
   const employees = [];
@@ -129,7 +132,6 @@ function updateLocalStorage() {
   localStorage.setItem("employees", JSON.stringify(employees));
 }
 
-// Function to load employees from local storage
 function loadFromLocalStorage() {
   const employees = JSON.parse(localStorage.getItem("employees")) || [];
   employees.forEach((employee) => {
@@ -137,7 +139,6 @@ function loadFromLocalStorage() {
   });
 }
 
-// Function to clear form fields
 function clearForm() {
   document.getElementById("id").value = "";
   document.getElementById("name").value = "";
@@ -148,7 +149,6 @@ function clearForm() {
   document.getElementById("dob").value = "";
 }
 
-// Function to delete an employee
 function deleteEmployee(id) {
   const tableBody = document.getElementById("tableBody");
   const tableRows = tableBody.querySelectorAll("tr");
@@ -158,42 +158,43 @@ function deleteEmployee(id) {
       updateLocalStorage();
     }
   });
+  clearForm();
 }
 
-// Function to update an employee
 function updateEmployee(id) {
+  clearForm();
   const tableBody = document.getElementById("tableBody");
   const tableRows = tableBody.querySelectorAll("tr");
 
-  // Find the employee with the given ID
   tableRows.forEach((row) => {
     if (row.cells[0].textContent === id.toString()) {
-      // Populate form fields with existing employee data
       document.getElementById("id").value = row.cells[0].textContent;
       document.getElementById("name").value = row.cells[1].textContent;
       const skills = row.cells[2].textContent.split(", ");
       skills.forEach((skill) => {
-        document.querySelector(`input[value="${skill}"]`).checked = true;
+        const checkbox = document.querySelector(`input[value="${skill}"]`);
+        if (checkbox) {
+          checkbox.checked = true;
+        }
       });
       document.getElementById("salary").value = row.cells[3].textContent;
       document.getElementById("dob").value = row.cells[4].textContent;
 
-      // Change form title and button text
-      document.querySelector("h1").innerHTML = "<u>Updating Form</u>";
+      document.getElementById("headingInsertOrUpdate").innerHTML = "<u>Updating Form</u>";
       document.getElementById("btn1").innerHTML = "Update";
       document.getElementById("btn1").setAttribute("onclick", `updateEmployeeData(${id})`);
     }
   });
 }
 
-// Function to update employee data
+
 function updateEmployeeData(id) {
   const name = document.getElementById("name").value;
   const skills = getSelectedSkills();
   const salary = document.getElementById("salary").value;
   const dob = document.getElementById("dob").value;
 
-  if (name && skills.length > 0 && salary && dob) {
+  if (name && salary && dob) {
     const employee = {
       id: id,
       name: name,
@@ -205,12 +206,10 @@ function updateEmployeeData(id) {
     addOrUpdateEmployee(employee);
     clearForm();
 
-    // Reset form title and button text to default values
-    document.querySelector("h1").innerHTML = "<u>Registration Form</u>";
+    document.getElementById("headingInsertOrUpdate").innerHTML = "<u>Registration Form</u>";
     document.getElementById("btn1").innerHTML = "Insert";
     document.getElementById("btn1").setAttribute("onclick", "submitForm(event)");
   }
 }
 
-// Load employees from local storage when the page is loaded
 document.addEventListener("DOMContentLoaded", loadFromLocalStorage);
